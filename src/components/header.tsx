88bbd5459b91e56b477515eeae4ff6cbba2738be
Ivan Stanojevic <ivan@byteout.com>
2019-09-06 13:56:34 +0200

    fix team page on mobile

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,6 +9,9 @@ export class Header extends React.Component<any, any> {
       <Navbar expand="lg">
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <NavLink className="nav-link" exact to="/">Home</NavLink>
+          </Nav>
           <Nav className="ml-auto">
             <NavLink className="nav-link" exact to="/">Home</NavLink>
           </Nav>
