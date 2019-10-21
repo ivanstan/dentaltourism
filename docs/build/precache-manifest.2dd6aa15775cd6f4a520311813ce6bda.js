0bd2adc6979021fc7c6cade95e9016d11b475bec
Ivan Stanojevic <ivanstan@gmail.com>
2019-10-21 17:52:48 +0200

    release

---
+++ docs/build/precache-manifest.2dd6aa15775cd6f4a520311813ce6bda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32e627a9302828b2688ab75e769a3ff5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46e07c6d5980fca1e961",
+    "url": "/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "8ef50915e163560b1ccf",
+    "url": "/static/css/main.c7264ea3.chunk.css"
+  },
+  {
+    "revision": "46e07c6d5980fca1e961",
+    "url": "/static/js/2.1bc29bd4.chunk.js"
+  },
+  {
+    "revision": "8ef50915e163560b1ccf",
+    "url": "/static/js/main.2b1e9577.chunk.js"
+  },
+  {
+    "revision": "4ce54c78376fa28ba93c",
+    "url": "/static/js/runtime~main.79b9b1d3.js"
+  }
+]);
