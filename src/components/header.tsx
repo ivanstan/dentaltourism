41de763ece538af900a7286f01e79b4c734e43a2
Ivan Stanojevic <ivanstan@gmail.com>
2019-11-10 19:59:23 +0100

    fix angrage

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,6 +4,24 @@ import {NavLink} from "react-router-dom";
 
 export class Header extends React.Component<any, any> {
 
+  public anfrageClick() {
+    window.location.hash = '#/';
+
+    setTimeout(() => {
+      let element = window.document.getElementById('anfrage');
+
+      if (element) {
+        let position = element.getBoundingClientRect();
+
+        window.scroll({
+          behavior: 'smooth',
+          left: 0,
+          top: position.top
+        });
+      }
+    }, 0);
+  }
+
   public render() {
     return <header>
       <Navbar expand="lg">
@@ -11,7 +29,7 @@ export class Header extends React.Component<any, any> {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto">
             <NavLink className="nav-link" exact to="/">Stratseite</NavLink>
-            <NavLink className="nav-link" exact to="/anfrage">Anfrage</NavLink>
+            <a className="nav-link" href="#" onClick={() => this.anfrageClick()}>Anfrage</a>
             <NavLink className="nav-link" exact to="/partners">Zahnarztpraxen</NavLink>
             <NavLink className="nav-link" exact to="/about">Über Serbien</NavLink>
             <NavLink className="nav-link" exact to="/contact">Kontakt</NavLink>
