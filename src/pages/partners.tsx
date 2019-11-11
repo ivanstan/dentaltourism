b485f252f7dcda6f8e93550acafb0048b6421077
Ivan Stanojevic <ivanstan@gmail.com>
2019-11-11 11:04:53 +0100

    add slider on partners page

--- src/pages/partners.tsx
+++ src/pages/partners.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
+import {Carousel} from "react-responsive-carousel";
 
 const video = {
   margin: 'auto',
-  width: 560,
+  width: '100%',
   display: 'block',
+  marginTop: 50,
 };
 
 export class Partners extends React.Component {
@@ -11,10 +13,30 @@ export class Partners extends React.Component {
     return <div className="container mb-5">
       <div className="row mb-5">
         <div className="col-12">
-          <h1 className="text-center mb-5">Zahnartzpraxen</h1>
-          <iframe style={video} width="560" height="315" src="https://www.youtube.com/embed/8UjsL7s99Xg" frameBorder="0"
-                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen></iframe>
+          <h1 className="text-center my-5">Zahnarztpraxis, Savski Venac</h1>
+          <div className="savski-venac-carousel">
+          <Carousel infiniteLoop={false} autoPlay={true} showThumbs={true} showStatus={true} stopOnHover={false}>
+            <div>
+              <img src="/images/ALN_1621.jpg" width="23%"/>
+            </div>
+            <div>
+              <img src="/images/ALN_1769.jpg" width="23%"/>
+            </div>
+            <div>
+              <img src="/images/ALN_1787.jpg" width="23%"/>
+            </div>
+            <div>
+              <img src="/images/ALN_1882.jpg" width="23%"/>
+            </div>
+          </Carousel>
+          </div>
+          <div className="embed-responsive embed-responsive-4by3">
+            <iframe style={video}
+                    className="embed-responsive-item"
+                    src="https://www.youtube.com/embed/8UjsL7s99Xg"
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen></iframe>
+          </div>
         </div>
       </div>
     </div>;
