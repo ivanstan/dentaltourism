1a695310088d366d285003e8486461bf72c6aa57
Ivan Stanojevic <ivanstan@gmail.com>
2019-11-11 11:11:50 +0100

    changes on partners page

--- src/pages/partners.tsx
+++ src/pages/partners.tsx
@@ -5,7 +5,6 @@ const video = {
   margin: 'auto',
   width: '100%',
   display: 'block',
-  marginTop: 50,
 };
 
 export class Partners extends React.Component {
@@ -13,9 +12,17 @@ export class Partners extends React.Component {
     return <div className="container mb-5">
       <div className="row mb-5">
         <div className="col-12">
-          <h1 className="text-center my-5">Zahnarztpraxis, Savski Venac</h1>
+          <h2 className="text-center my-5">Zahnarztpraxis Belgrad, Savamala</h2>
+          <div className="embed-responsive embed-responsive-4by3">
+            <iframe style={video}
+                    className="embed-responsive-item"
+                    src="https://www.youtube.com/embed/8UjsL7s99Xg"
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen></iframe>
+          </div>
+          <h2 className="text-center my-5">Zahnarztpraxis Belgrad, Savski Venac</h2>
           <div className="savski-venac-carousel">
-          <Carousel infiniteLoop={false} autoPlay={true} showThumbs={true} showStatus={true} stopOnHover={false}>
+          <Carousel infiniteLoop={false} autoPlay={false} showThumbs={true} showStatus={true} stopOnHover={false}>
             <div>
               <img src="/images/ALN_1621.jpg" width="23%"/>
             </div>
@@ -29,13 +36,6 @@ export class Partners extends React.Component {
               <img src="/images/ALN_1882.jpg" width="23%"/>
             </div>
           </Carousel>
-          </div>
-          <div className="embed-responsive embed-responsive-4by3">
-            <iframe style={video}
-                    className="embed-responsive-item"
-                    src="https://www.youtube.com/embed/8UjsL7s99Xg"
-                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen></iframe>
           </div>
         </div>
       </div>
