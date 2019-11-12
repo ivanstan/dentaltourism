262a5ba86f81005f19d2596f28de5e4d53a27207
tonizub <57598438+tonizub@users.noreply.github.com>
2019-11-12 20:21:21 +0100

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -20,15 +20,15 @@ export class About extends React.Component {
           <div className="col-12">
             <h1 className="mt-5">Über Serbien</h1>
             <p>
-              Serbien ist ein Staat in Südosteuropa.Sehr warme Sommer und kalte Winter sind für dieses Land
-              charakteristisch.Belgrad als Hauptstadt mit über 2 Millionen Einwohnern, liegt an zwei Flüssen, Donau und
-              Sava.Dies macht die Stadt höchstwahrscheinlich sehr beliebt, um in ihr zu wohnen.Belgrad ist eine sehr lebendige Stadt mit
-              verschiedenen Universitäten, Museen und vielen Sehenswürdigkeiten.Einige davon sind der Kalemegdan, eine Festung aus
-              Mittelalter, das Nikola Tesla Museum, die Ada Ciganlija, der Sava See usw.Desweiteren gibt es sehr gut geführte Gaststätten, die
+              Serbien ist ein Staat in Südosteuropa. Sehr warme Sommer und kalte Winter sind für dieses Land
+              charakteristisch. Belgrad als Hauptstadt mit über 2 Millionen Einwohnern, liegt an zwei Flüssen, Donau und
+              Sava. Dies macht die Stadt höchstwahrscheinlich sehr beliebt, um in ihr zu wohnen. Belgrad ist eine sehr lebendige Stadt mit
+              verschiedenen Universitäten, Museen und vielen Sehenswürdigkeiten. Einige davon sind der Kalemegdan, eine Festung aus dem
+              Mittelalter, das Nikola Tesla Museum, die Ada Ciganlija, der Sava See usw.. Desweiteren gibt es sehr gut geführte Gaststätten, die
               besonders am Wasser in Form von schwimmenden Booten beliebt und besuchenswert sind.
-              In Belgrad befindet sich der Flughafen Nikola Tesla. Dieser wird täglich von der
+              In Belgrad befindet sich der Flughafen Nikola Tesla. Dieser wird täglich von den
               kostengünstigeren Unternehmen wie Ryan- und Wizzair von Deutschland aus angeflogen.
-              Dasselbe gilt für 200 Km entfernte Stadt Niš und deren Flughafen.
+              Dasselbe gilt für die 200 Km entfernte Stadt Niš und deren Flughafen.
             </p>
           </div>
         </div>
